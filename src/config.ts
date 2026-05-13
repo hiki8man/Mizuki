35f29d2cc710f3c66960aaae7e63c52d24b0da9b
hiki8man <63759994+hiki8man@users.noreply.github.com>
2026-05-13 09:41:32 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "安定の小屋",
 	subtitle: "随便记点东西",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://blog.hiki8man.top/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-05-12", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
